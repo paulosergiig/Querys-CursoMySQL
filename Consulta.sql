bc68cde455891be3bfdc202eb70f68372488d031
Paulo Sérgio <paulosergiig@gmail.com>
2026-03-28 14:52:19 -0300

    teste commit

---
+++ Consulta.sql
@@ -0,0 +1,9 @@
+USE db_locadora;
+-- Insert unico
+--INSERT INTO tb_categoria (descricao) VALUES ('Ficção científica');
+--INSERT INTO tb_categoria (descricao) VALUES ('Terror');
+
+-- Insert aninhado
+INSERT INTO tb_categoria (descricao) VALUES ('RPG 1'),('RPG 2'),('RPG 3');
+
+
