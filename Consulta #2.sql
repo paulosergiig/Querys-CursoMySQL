e7ade9a2e9daa7d6b3a932086a79a663f6aa0eba
Paulo Sérgio <paulosergiig@gmail.com>
2026-03-28 15:13:07 -0300

    commit 2

---
+++ Consulta #2.sql
@@ -0,0 +1,13 @@
+USE db_locadora;
+
+INSERT INTO tb_clientes (nome, telefone, email) VALUES
+('Paulo', '88888881', 'paulo1@email.com'),
+('Paulo', '88888882', 'paulo2@email.com'),
+('Paulo', '88888883', 'paulo3@email.com'),
+('Paulo', '88888884', 'paulo4@email.com'),
+('Paulo', '88888885', 'paulo5@email.com'),
+('Paulo', '88888886', 'paulo6@email.com'),
+('Paulo', '88888887', 'paulo7@email.com'),
+('Paulo', '88888888', 'paulo8@email.com'),
+('Paulo', '88888889', 'paulo9@email.com'),
+('Paulo', '888888810', 'paulo10@email.com');
